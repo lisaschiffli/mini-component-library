9ac4a02b7ea4367c8b8f2eba08565f82976e6d05
lisaschiffli <lisaschiffli@users.noreply.github.com>
2021-06-30 17:19:42 -0400

    ProgressBar complete

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,23 +5,88 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+	small: {
+		height: 8,
+		padding: 0,
+		radius: 4
+	},
+	medium: {
+		height: 12,
+		padding: 0,
+		radius: 4
+	},
+	large: {
+		height: 16,
+		padding: 4,
+		radius: 8
+	}
+};
+
 const ProgressBar = ({ value, size }) => {
+	const styles = STYLES[size];
+
+	if (!styles) {
+		throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+	}
+
 	return (
-		<BarWrapper size>
-			<Progress value />
-		</BarWrapper>
+		<Wrapper
+			role='progressbar'
+			aria-valuenow={value}
+			aria-valuemin='0'
+			aria-valuemax='100'
+			style={{
+				'--padding': styles.padding + 'px',
+				'--radius': styles.radius + 'px'
+			}}
+		>
+			<VisuallyHidden>{value}%</VisuallyHidden>
+			<BarWrapper>
+				<Bar
+					style={{
+						'--width': value + '%',
+						'--height': styles.height + 'px'
+					}}
+				/>
+			</BarWrapper>
+		</Wrapper>
 	);
 };
 
-const BarWrapper = styled.div`
-	width: 100%;
+const Wrapper = styled.div`
 	background-color: ${COLORS.transparentGray15};
+	box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+	border-radius: var(--radius);
+	padding: var(--padding);
 `;
 
-const Progress = styled.div`
-	width: 20%;
-	height: 30px;
-	background-color: ${COLORS.primary};
+const BarWrapper = styled.div`
+	border-radius: 4px;
+	/* Trim off corners when progress bar is near-full.  */
+	overflow: hidden;
 `;
+
+const Bar = styled.div`
+	width: var(--width);
+	height: var(--height);
+	background-color: ${COLORS.primary};
+	border-radius: 4px 0 0 4px;
+`;
+// const BarWrapper = styled.div`
+// 	position: absolute;
+// 	width: 370px;
+// 	height: var(--height);
+// 	background-color: ${COLORS.transparentGray15};
+// 	border-radius: var(--borderRadius);
+// `;
+
+// const Progress = styled.progress`
+// 	background-color: ${COLORS.primary};
+// 	position: absolute;
+// 	width: 370px;
+// 	height: var(--height);
+// 	border-radius: var(--borderRadius);
+// `;
 
 export default ProgressBar;
